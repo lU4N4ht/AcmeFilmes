d8f819d00264a68a32436dd502938dd83f276bbd
Luana <}luana.maga78@gmail.com>
2024-01-23 17:15:29 -0300

    faltando testar os endpoints no postman

---
+++ app.js
@@ -0,0 +1,72 @@
+/*********************************************************************************************************************************************************
+ * Objetivo: Criar uma API para responder dados da pizzaria
+ * Data: 10/12/2023
+ * Autor: Luana
+ * Versão: 1.0
+ ********************************************************************************************************************************************************/
+
+/***************************
+ * Instalações para a API
+ *  npm install [] --save
+ *  express - Dependencia do node para auxiliar na criação de API
+ *  cors - Dependencia do node para manipular recursos de acesso, permissões, etc da API (HEADER)
+ *  body-parser - Dependencia do node para auxiliar na chegada de dados na API (BODY)
+ * 
+ * Quatro métodos:
+ * Get - Pegar dados;
+ * Post - Envia dados novos;
+ * Put - Altera dados existentes;
+ * Delete - Apaga dados existentes;
+ ***************************/
+
+//Importando as bibliotecas instaladas para o projeto
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { request } = require('http');
+
+
+//Instanciando o express (cria um objeto "app" tendo referencia a classe do express)
+const app = express();
+
+//Função para configurar as permissões do cors
+app.use((request, response, next) => {
+    //Configura quem poderá fazer requisições na API (* - libera para todos | IP restringe o acesso)
+    response.header('Access-Control-Allow-Origin', '*')
+    //Libera/Configura os métodos que poderão ser usados na API
+    response.header('Access-Control-Allow-Methods', 'Get')
+    
+    app.use(cors());
+
+    //Faz com que ele de sequência, indo pra próxima função
+    next();
+
+})
+
+//EndPoint: Listar todos os filmes;
+app.get('/v1/AcmeFilmes/ListarFilmes', cors(), async function(request, response, next){
+    let controleListaFilmes = require('./controller/function.js')
+    let filmes = controleListaFilmes.getListaDeFilmes();
+    response.json(filmes);
+    response.status(200);
+});
+
+//Endpoint: Listar os filmes e suas informações com base em um critério(id)
+app.get('/v1/AcmeFilmes/ListarFilmes', cors(), async function(request, response, next){
+    let id = request.query.id
+    let controleListaFilmes = require('./controller/function.js')
+    let filmes = controleListaFilmes.getFilme(id);
+
+    if(filmes){
+        response.json(filmes);
+        response.status(200);
+    } else{
+        response.status(404)
+        response.json({erro:'Não foi possível encontrar um item.'})
+    }
+});
+
+//Executando a API e fazendo ela ficar aguardando requisições
+app.listen(8080, function(){
+    console.log('API funcionando e aguardando requisições.')
+});
---
+++ model/filmes.js
@@ -0,0 +1,158 @@
+var filmes = {
+  "filmes": [
+    {
+      "id": 1,
+      "nome": "O Segredo do Vale",
+      "sinopse": "Um drama emocionante que explora os segredos de uma pequena cidade no interior.",
+      "duracao": "2 horas e 15 minutos",
+      "data_lancamento": "2022-08-25",
+      "data_relancamento": null,
+      "foto_capa": "https://exemplo.com/foto_vale.jpg",
+      "valor_unitario": 29.99
+    },
+    {
+      "id": 2,
+      "nome": "A Herança Perdida",
+      "sinopse": "Um filme de suspense que segue a jornada de um herdeiro em busca de seu passado misterioso.",
+      "duracao": "2 horas e 30 minutos",
+      "data_lancamento": "2023-03-12",
+      "data_relancamento": "2023-10-05",
+      "foto_capa": "https://exemplo.com/foto_heranca.jpg",
+      "valor_unitario": 24.99
+    },
+    {
+      "id": 3,
+      "nome": "A Viagem no Tempo",
+      "sinopse": "Uma aventura sci-fi que leva os espectadores a uma jornada através das eras.",
+      "duracao": "2 horas",
+      "data_lancamento": "2024-01-07",
+      "data_relancamento": null,
+      "foto_capa": "https://exemplo.com/foto_tempo.jpg",
+      "valor_unitario": 34.99
+    },
+    {
+      "id": 4,
+      "nome": "O Enigma do Espaço",
+      "sinopse": "Uma trama intrigante que explora os mistérios do espaço sideral.",
+      "duracao": "2 horas e 10 minutos",
+      "data_lancamento": "2024-05-15",
+      "data_relancamento": null,
+      "foto_capa": "https://exemplo.com/foto_espaco.jpg",
+      "valor_unitario": 27.99
+    },
+    {
+      "id": 5,
+      "nome": "Caminhos Cruzados",
+      "sinopse": "Um filme de romance que narra a história de amores entrelaçados em destinos cruzados.",
+      "duracao": "2 horas e 5 minutos",
+      "data_lancamento": "2023-09-28",
+      "data_relancamento": null,
+      "foto_capa": "https://exemplo.com/foto_caminhos.jpg",
+      "valor_unitario": 22.99
+    },
+    {
+      "id": 6,
+      "nome": "A Cidade Esquecida",
+      "sinopse": "Um thriller urbano que revela os segredos sombrios de uma metrópole esquecida.",
+      "duracao": "2 horas e 20 minutos",
+      "data_lancamento": "2024-02-18",
+      "data_relancamento": "2024-09-10",
+      "foto_capa": "https://exemplo.com/foto_cidade.jpg",
+      "valor_unitario": 31.99
+    },
+    {
+      "id": 7,
+      "nome": "O Mistério da Ilha",
+      "sinopse": "Uma aventura insular que desvenda os enigmas de uma ilha remota.",
+      "duracao": "2 horas e 25 minutos",
+      "data_lancamento": "2023-06-08",
+      "data_relancamento": null,
+      "foto_capa": "https://exemplo.com/foto_ilha.jpg",
+      "valor_unitario": 26.99
+    },
+    {
+      "id": 8,
+      "nome": "Memórias Fragmentadas",
+      "sinopse": "Um drama psicológico que mergulha nas memórias fragmentadas de seus personagens.",
+      "duracao": "2 horas e 15 minutos",
+      "data_lancamento": "2024-07-20",
+      "data_relancamento": null,
+      "foto_capa": "https://exemplo.com/foto_memorias.jpg",
+      "valor_unitario": 29.99
+    },
+    {
+      "id": 9,
+      "nome": "O Jogo da Mente",
+      "sinopse": "Um thriller psicológico que desafia os limites da mente humana em um jogo de inteligência.",
+      "duracao": "2 horas e 30 minutos",
+      "data_lancamento": "2023-12-05",
+      "data_relancamento": "2024-06-18",
+      "foto_capa": "https://exemplo.com/foto_jogo.jpg",
+      "valor_unitario": 33.99
+    },
+    {
+      "id": 10,
+      "nome": "A Conspiração Global",
+      "sinopse": "Um filme de suspense que revela uma conspiração que abrange todo o globo.",
+      "duracao": "2 horas e 10 minutos",
+      "data_lancamento": "2024-03-30",
+      "data_relancamento": null,
+      "foto_capa": "https://exemplo.com/foto_conspiracao.jpg",
+      "valor_unitario": 28.99
+    },
+    {
+      "id": 11,
+      "nome": "O Último Refúgio",
+      "sinopse": "Um drama que retrata a busca por um último refúgio em um mundo em transformação.",
+      "duracao": "2 horas e 15 minutos",
+      "data_lancamento": "2023-01-15",
+      "data_relancamento": null,
+      "foto_capa": "https://exemplo.com/foto_refugio.jpg",
+      "valor_unitario": 29.99
+    },
+    {
+      "id": 12,
+      "nome": "Além do Horizonte",
+      "sinopse": "Uma jornada épica além dos limites conhecidos, explorando o desconhecido.",
+      "duracao": "2 horas e 20 minutos",
+      "data_lancamento": "2024-09-02",
+      "data_relancamento": null,
+      "foto_capa": "https://exemplo.com/foto_horizonte.jpg",
+      "valor_unitario": 31.99
+    },
+    {
+      "id": 13,
+      "nome": "O Poder da Natureza",
+      "sinopse": "Um documentário que explora a majestade e a força da natureza em todo o mundo.",
+      "duracao": "2 horas",
+      "data_lancamento": "2023-04-17",
+      "data_relancamento": null,
+      "foto_capa": "https://exemplo.com/foto_natureza.jpg",
+      "valor_unitario": 24.99
+    },
+    {
+      "id": 14,
+      "nome": "A Última Fronteira",
+      "sinopse": "Uma expedição emocionante em busca da última fronteira inexplorada.",
+      "duracao": "2 horas e 25 minutos",
+      "data_lancamento": "2024-01-30",
+      "data_relancamento": null,
+      "foto_capa": "https://exemplo.com/foto_fronteira.jpg",
+      "valor_unitario": 26.99
+    },
+    {
+      "id": 15,
+      "nome": "O Despertar da Magia",
+      "sinopse": "Um conto mágico que revela o despertar de poderes extraordinários em um mundo comum.",
+      "duracao": "2 horas e 10 minutos",
+      "data_lancamento": "2023-07-12",
+      "data_relancamento": "2024-04-05",
+      "foto_capa": "https://exemplo.com/foto_magia.jpg",
+      "valor_unitario": 27.99
+    }
+  ]
+}
+
+module.exports = {
+  filmes
+}
