8586e85728aa94dd07b872f8d2c9a90f14692b6a
lU4N4ht <luana.maga78@gmail.com>
2024-03-26 19:15:32 -0300

    O DEPLOY FOOOOOOOOOOOOOOOOI

--- prisma/migrations/20240215185422_backup15022024/migration.sql
+++ prisma/migrations/20240215185422_backup15022024/migration.sql
@@ -2,6 +2,6 @@
 CREATE TABLE `tbl_test` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `nome` VARCHAR(191) NOT NULL,
-
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
